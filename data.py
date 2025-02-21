d01722398b181467c3d1faaf78a1d5e3a0883306
samehaisaa <a.semeh555@gmail.com>
2025-02-21 14:27:02 +0000

    Add generate_data_normal function for Riemannian normal distribution

--- data.py
+++ data.py
@@ -3,13 +3,20 @@ import geomstats.backend as gs
 from geomstats.geometry.hypersphere import Hypersphere
 from geomstats.learning.frechet_mean import FrechetMean
 from geomstats.learning.pca import TangentPCA
-from config import N_SAMPLES, KAPPA
+from config import N_SAMPLES, KAPPA,PRECISION
 
 def generate_data(n_samples=N_SAMPLES, kappa=KAPPA):
     sphere = Hypersphere(dim=2)
     random_mu = np.random.randn(3)
     random_mu /= np.linalg.norm(random_mu)
     data = sphere.random_von_mises_fisher(kappa=kappa, n_samples=n_samples, mu=gs.array(random_mu))
+    return sphere, data
+
+def generate_data_normal(n_samples=N_SAMPLES, precision=PRECISION):
+    sphere = Hypersphere(dim=2)
+    random_mu = np.random.randn(3)
+    random_mu /= np.linalg.norm(random_mu)
+    data = sphere.random_riemannian_normal(mean=random_mu,precision=precision, n_samples=n_samples)
     return sphere, data
 
 def compute_frechet_mean(sphere, data):
